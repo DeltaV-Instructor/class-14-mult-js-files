25136de72ffc5951de35c6f4f83d377eac5613fc
tektechnologies <info@tektechnologies.com>
2023-09-28 11:48:27 -0500

    get local storage render to both pages

--- js/appOne.js
+++ js/appOne.js
@@ -4,7 +4,15 @@ console.log('appOne js is connected');
 
 //grab local storage and render to page
 
-
+if(localStorage.pizzas){
+  //can we get some local storage out
+  console.log(localStorage.pizzas);
+  let pizzaFromLS = JSON.parse(localStorage.pizzas);
+  for(let i = 0; i < pizzaFromLS.length; i++){
+    new Pizza(pizzaFromLS[i].pizzaType);
+    allPizzas[i].render();
+  }
+}
 
 
 
--- js/appTwo.js
+++ js/appTwo.js
@@ -1,2 +1,26 @@
 'use strict';
 console.log('appTwo js is connected');
+
+
+
+let pizzaButton = document.getElementById('pizzaButton');
+console.log(pizzaButton);
+
+
+//function expression
+let handlePizzaButtonClick = function(){
+  //parse our local storage
+  let pizzasFromLS = JSON.parse(localStorage.pizzas);
+  console.log(pizzasFromLS);
+  //loop over our local and turn our objects back into Pizza objects by using our constructor function.
+  for(let i = 0; i < pizzasFromLS.length; i++){
+    let newPizza = new Pizza(pizzasFromLS[i].pizzaType);
+    newPizza.render();
+  }
+  //render to the list on the second page.
+  console.log(allPizzas);
+
+};
+
+
+pizzaButton.addEventListener('click', handlePizzaButtonClick);
